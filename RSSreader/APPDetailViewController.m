96deff46646c02d91c3ecdb2935c591706429c3c
wahlix <rickardwahlander@Rickards-MacBook-Air.local>
2014-08-05 20:54:51 +0200

    All set for standard
    Standard

---
+++ RSSreader/APPDetailViewController.m
@@ -0,0 +1,23 @@
+//
+//  APPDetailViewController.m
+//  RSSreader
+//
+//  Created by Rafael Garcia Leiva on 08/04/13.
+//  Copyright (c) 2013 Appcoda. All rights reserved.
+//
+
+#import "APPDetailViewController.h"
+
+@implementation APPDetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSURL *myURL = [NSURL URLWithString: [self.url stringByAddingPercentEscapesUsingEncoding:
+                                          NSUTF8StringEncoding]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:myURL];
+    [self.webView loadRequest:request];
+}
+
+@end
